9b5fc37da2df5be93e7ac69b419b6688a6a8c687
tdslinden <linden-k@hotmail.com>
2021-04-07 21:58:55 -0700

    initial commit

---
+++ interface-v5.rs
@@ -0,0 +1,90 @@
+use std::fmt::Debug;
+
+#[derive(Debug)]
+/// A struct that represents a key-value store.
+pub struct KVStore {
+    /// The number of key-value mappings currently stored.
+    size: usize,
+    /// The location of the file system where key-value mappings are stored.
+    path: String,
+}
+
+/// A trait that defines the operations that need to be supported.
+pub trait Operations {
+    /// A function that initializes a KVStore instance.
+    ///
+    /// If there is no directory at the provided path, this should create it. If there is an error
+    /// while creating a directory, this should return an [std::io::Error].
+    ///
+    /// If there are **no** key-value mappings stored already under the directory, this
+    /// should simply create a new KVStore instance that can store and retrieve key-value mappings
+    /// using the directory. It should also correctly initialize the size to 0.
+    ///
+    /// If there **are** existing key-value mappings stored already under the directory, this
+    /// should initialize a KVStore instance that is able to store and retrieve existing key-value
+    /// mappings as well as new key-value mappings. It should also correctly initialize the size to
+    /// the number of existing key-value mappings.
+    fn new(path: &str) -> std::io::Result<Self>
+    where
+        Self: Sized;
+
+    /// A function that returns the number of key-value mappings currently stored.
+    fn size(self: &Self) -> usize;
+
+    /// A function that inserts a new key-value mapping.
+    ///
+    /// If there is **no** key-value mapping stored already with the same key, it should return
+    /// `Ok(())` if storing is successfully done.
+    ///
+    /// If there **is** a key-value mapping stored already with the same key, it should return an
+    /// [std::io::Error].
+    ///
+    /// Make sure you read and understand the assignment document regarding how to store key-value
+    /// mappings using files as well as how to structure sub-directories.
+    ///
+    /// Make sure you understand what the trait bounds mean for K and V.
+    ///
+    /// Refer to [https://docs.serde.rs/serde/](https://docs.serde.rs/serde/)
+    /// and [https://serde.rs](https://serde.rs) for serde.
+    fn insert<K, V>(self: &mut Self, key: K, value: V) -> std::io::Result<()>
+    where
+        K: serde::Serialize + Default + Debug,
+        V: serde::Serialize + Default + Debug;
+
+    /// A function that returns a previously-inserted value.
+    ///
+    /// If there **is** a key-value mapping stored already with the same key, it should return
+    /// the value.
+    ///
+    /// If there is **no** key-value mapping stored already with the same key, it should return
+    /// an [std::io::Error].
+    ///
+    /// Make sure you understand what the trait bounds mean for K and V.
+    ///
+    /// Refer to [https://docs.serde.rs/serde/](https://docs.serde.rs/serde/)
+    /// and [https://serde.rs](https://serde.rs) for serde.
+    fn lookup<K, V>(self: &Self, key: K) -> std::io::Result<V>
+    where
+        K: serde::Serialize + Default + Debug,
+        V: serde::de::DeserializeOwned + Default + Debug;
+
+    /// A function that removes a previously-inserted key-value mapping.
+    ///
+    /// If there **is** a key-value mapping stored already with the same key, it should return
+    /// the value and delete the key-value mapping from the file system.
+    ///
+    /// If there is **no** key-value mapping stored already with the same key, it should
+    /// return an [std::io::Error].
+    ///
+    /// If a sub-directory does not contain any key-value files, this should delete the
+    /// sub-directory as well.
+    ///
+    /// Make sure you understand what the trait bounds mean for K and V.
+    ///
+    /// Refer to [https://docs.serde.rs/serde/](https://docs.serde.rs/serde/)
+    /// and [https://serde.rs](https://serde.rs) for serde.
+    fn remove<K, V>(self: &mut Self, key: K) -> std::io::Result<V>
+    where
+        K: serde::Serialize + Default + Debug,
+        V: serde::de::DeserializeOwned + Default + Debug;
+}
