8c125a90dbd7b797aea5878799dfb6f42da50b99
tdslinden <linden-k@hotmail.com>
2021-04-13 17:08:20 -0700

    Add dependencies to Cargo.toml file and include partial implementation to the insert function.

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@ use std::error::Error;
 use serde::{Serialize, Deserialize};
 use std::path::Path;
 use std::fs;
-#[derive(Debug)]
+
+
+#[derive(Serialize, Deserialize, Debug)]
 /// A struct that represents a key-value store.
 pub struct KVStore {
     /// The number of key-value mappings currently stored.
@@ -53,43 +55,6 @@ pub trait Operations {
     where
         K: serde::Serialize + Default + Debug,
         V: serde::Serialize + Default + Debug;
-
-    /// A function that returns a previously-inserted value.
-    ///
-    /// If there **is** a key-value mapping stored already with the same key, it should return
-    /// the value.
-    ///
-    /// If there is **no** key-value mapping stored already with the same key, it should return
-    /// an [std::io::Error].
-    ///
-    /// Make sure you understand what the trait bounds mean for K and V.
-    ///
-    /// Refer to [https://docs.serde.rs/serde/](https://docs.serde.rs/serde/)
-    /// and [https://serde.rs](https://serde.rs) for serde.
-    fn lookup<K, V>(self: &Self, key: K) -> std::io::Result<V>
-    where
-        K: serde::Serialize + Default + Debug,
-        V: serde::de::DeserializeOwned + Default + Debug;
-
-    /// A function that removes a previously-inserted key-value mapping.
-    ///
-    /// If there **is** a key-value mapping stored already with the same key, it should return
-    /// the value and delete the key-value mapping from the file system.
-    ///
-    /// If there is **no** key-value mapping stored already with the same key, it should
-    /// return an [std::io::Error].
-    ///
-    /// If a sub-directory does not contain any key-value files, this should delete the
-    /// sub-directory as well.
-    ///
-    /// Make sure you understand what the trait bounds mean for K and V.
-    ///
-    /// Refer to [https://docs.serde.rs/serde/](https://docs.serde.rs/serde/)
-    /// and [https://serde.rs](https://serde.rs) for serde.
-    fn remove<K, V>(self: &mut Self, key: K) -> std::io::Result<V>
-    where
-        K: serde::Serialize + Default + Debug,
-        V: serde::de::DeserializeOwned + Default + Debug;
 }
 
 impl Operations for KVStore {
@@ -119,8 +84,10 @@ impl Operations for KVStore {
     }
 
     fn insert<K, V>(self: &mut Self, key: K, value: V) -> std::io::Result<()> {
-        //serde a key, create a SHA from that, store in directory
-
+        //serde a key, create a SHA for the key and the value, use filewriter to store in given path directory
+        
+        let serialize_key = serde_json::to_string(key).unwrap();
+        Ok(())
     }
 
     // fn lookup<K, V>(self: &Self, key: K) -> std::io::Result<V> {
